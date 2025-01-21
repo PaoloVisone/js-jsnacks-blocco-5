435111ca085d1f0404f580e66abcabac5638d6e8
PaoloVisone <polvis.pv.22@gmail.com>
2025-01-21 15:14:30 +0100

    Ex. 4 completato

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,9 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const nomi = posts.map(element => {
+  return element.author
+});
+
+console.log(nomi);
